5e5d81a911c9623cc1a5a19c641d2b35a4df5153
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-12-07 17:11:34 +0700

    Resolve Course API conflict

--- src/lib/api/courseAPI.ts
+++ src/lib/api/courseAPI.ts
@@ -8,37 +8,22 @@ import {
 //import { terminal } from "virtual:terminal"; // for debugging
 
 export const courseAPI = {
+  getCourses: async () => {
+    return apiClient.get("course/courses");
+  },
+
+  search: async (keyword: string) => {
+    return apiClient.get("course/courses/search?keyword=" + keyword);
+  },
+
   getCourseDetail: async (courseId: string) => {
     const response: IGetCourseDetailResponse = await apiClient.get(`/course/courses/${courseId}`);
     return response;
-    // return {
-    //   "code": 0,
-    //   "result": {
-    //     "id": "e2148e8e-e706-4338-b445-afe406e1c90d",
-    //     "name": "C++ For Beginners - 1",
-    //     "description": "Basic knowledge of C++ programming language",
-    //     "level": "Beginner",
-    //     "userEnrolled": false
-    //   }
-    // } as IGetCourseDetailResponse;
   },
 
   getLessons: async (courseId: string) => {
     const response: IGetCourseLessonsResponse = await apiClient.get(`/course/courses/${courseId}/lessons`);
     return response;
-    // return {
-    //   "code": 0,
-    //   "result": [
-    //     {
-    //       "id": "c7a7e6bd-a5ba-4d13-97fe-db0144524dd9",
-    //       "lessonOrder": 1,
-    //       "name": "The First C++ Program",
-    //       "description": "Hello World!",
-    //       "content": "# The Hello Word program\n Some code sample here\n # Explanation\n Some explanation here\n",
-    //       "courseId": "8ab6ad07-aa9d-4ca1-9680-72e2e98bbb81"
-    //     }
-    //   ] as ILesson[]
-    // } as IGetCourseLessonsResponse;
   },
 
   enrollCourse: async (useUid: string, courseId: string) => {
@@ -47,27 +32,10 @@ export const courseAPI = {
       courseId: courseId
     });
     return response;
-
-    // return {
-    //   "id": 0,
-    //   "userUid": "123",
-    //   "courseIds": ["e2148e8e-e706-4338-b445-afe406e1c90d"]
-    // } as IEnrollCourseResponse
   },
 
   getLessonDetail: async (lessonId: string) => {
     const response: IGetLessonDetailResponse = await apiClient.get(`/course/lessons/${lessonId}`);
     return response;
-    // return {
-    //   "code": 0,
-    //   "result": {
-    //     "id": "c7a7e6bd-a5ba-4d13-97fe-db0144524dd9",
-    //     "lessonOrder": 1,
-    //     "name": "The First C++ Program",
-    //     "description": "Hello World!",
-    //     "content": "# The Hello Word program\n ## Code sample\n ```A code block sample here```\n ## Explanation\n - Some explanation here\n - Some conclusion here\n # Summary\n - Some summary here\n",
-    //     "courseId": "8ab6ad07-aa9d-4ca1-9680-72e2e98bbb81"
-    //   } as ILesson
-    // } as IGetLessonDetailResponse;
   }
 };
