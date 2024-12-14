276787f7f61abd8fa0d2ff8aeadc9c8e09d43e88
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-12-14 10:53:49 +0700

    package: remove unnecessary logging package

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import Terminal from 'vite-plugin-terminal'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), Terminal()],
+  plugins: [react()],
   server: {
     port: 3000
   },
